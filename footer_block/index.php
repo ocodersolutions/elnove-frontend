ee3aaf02fdf956bcd10b561eeb58e2dde0f89026
Quang Tran <topinfo93@gmail.com>
2016-08-02 11:35:26 +0700

    footer update

--- footer_block/index.php
+++ footer_block/index.php
@@ -1,11 +1,60 @@
 <!DOCTYPE html>
 <html class="no-js">
     <head>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.google.com/fonts#UsePlace:use/Collection:Robotohttps://www.google.com/fonts#UsePlace:use/Collection:Roboto" media="all">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet/less" type="text/css" href="../bk/assets/css/quang.less" />
+    <script   src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/2.6.1/less.min.js"></script>
 
     </head>
     <body>
-        <div class="html-addon-name">
+       	<!-- Footer -->
+		<footer>
+		    <div class="line-footer"></div>
+			    <div class="footer-above">
+			    	<div class="menu-footer col-xs-6">
+				        <div class="list-menu">
+				            <ul>
+				                <li>How it work</li>
+				                <li>About Us</li>
+				                <li>Blog</li>
+				                <li>FAQ</li>
+				                <li>Shipping</li>
+				                <li>Pricing</li>
+				                <li>Exchange & Return</li>
+				                <li>Careers</li>
+				                <li>Contact Us</li>
+				            </ul>
+				        </div>
+				    </div>
 
-        </div>
+				    <div class="newsletter col-xs-4">
+			            <div class="newsletter-input ">
+			                <span>Sign up for newsletter</span>
+			                <input placehoder="Your Email Address Here" >
+			            </div>
+			        </div>
+
+			        <div class="footer-social col-xs-2">
+			            <div class="social-block">
+			            <div class="social" ><i class="fa fa-facebook" aria-hidden="true"></i></div>
+			            <div class="social" ><i class="fa fa-twitter" aria-hidden="true"></i></div>
+			            <div class="social" ><i class="fa fa-instagram" aria-hidden="true"></i></div>
+			            <div class="social" ><i class="fa fa-youtube" aria-hidden="true"></i></div>
+			          
+			            </div>
+			        </div>
+			    </div>
+			    <div class="col-xs-12 copyright">
+			        <span class="copyright-text"> COPYRIGHT 2016 STITCH STYLE SOLUTIONS INC. ALL RIGHTS RESERVED</span>
+			        <span class="copyright-text">| TERMS &amp; CONDITIONS</span>
+			        <span class="copyright-text">| PRIVACY &amp; POLICY</span>
+			    </div>
+		    
+		</footer>
+		<!-- End-Footer -->
     </body>
 </html>
