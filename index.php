605d76b9af00b3d35096826118721b5e229135e2
Hoang Trung <hoangtrungweb@gmail.com>
2016-08-02 14:37:46 +0700

    Build struct of project

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="favicon.ico">
+
+        <title>eLnove.com</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="assets/css/custom.less" rel="stylesheet/less" type="text/css">
+        <link href="assets/css/custom_header.less" rel="stylesheet/less" type="text/css">
+        <link href="assets/css/custom_banner.less" rel="stylesheet/less" type="text/css">
+        <link href="assets/css/custom_refine.less" rel="stylesheet/less" type="text/css">
+        <link href="assets/css/custom_product_list.less" rel="stylesheet/less" type="text/css">
+        <link href="assets/css/custom_footer.less" rel="stylesheet/less" type="text/css">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <div id="header-container" class="container-fluid">
+            <?php include 'header_block/index.php'; ?>
+        </div>
+        <div id="banner-container" class="container-fluid">
+            <?php include 'banner_block/index.php'; ?>
+        </div>
+        <div id="body-container" class="container-fluid">
+            
+        </div>
+        <footer class="container-fluid">
+            <?php include 'footer_block/index.php'; ?>
+        </footer>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/2.6.1/less.min.js"></script>
+  </body>
+</html>
