f48c395402ccdd90a5629133451b0720d06da2b7
Quang Tran <topinfo93@gmail.com>
2016-08-03 10:42:03 +0700

    update refine

--- footer_block/index.php
+++ footer_block/index.php
@@ -16,7 +16,7 @@
 			<div class="row">
 			    <div class="line-footer"></div>
 				    <div class="footer-above">
-				    	<div class="menu-footer col-lg-6 ">
+				    	<div class="menu-footer col-lg-6">
 					        <div class="list-menu">
 					            <ul>
 					                <li>How it work</li>
@@ -33,10 +33,12 @@
 					    </div>
 
 					    <div class="newsletter col-lg-4">
-				            <div class="newsletter-input ">
+				            <div class="newsletter-wrap ">
 				                <span>Sign up for newsletter</span>
+				                <div class="newsletter-input ">
 				                <input placeholder="Your Email Address Here" >
 				                <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+				                </div>
 				            </div>
 				        </div>
 
@@ -58,9 +60,7 @@
 					        <span class="copyright-text">| &nbsp; &nbsp; PRIVACY &amp; POLICY</span>
 					    </div>
 					</div>	
-					
 			</div>
-		    
 		</footer>
 		<!-- End-Footer -->
     </body>
--- refine_block/index.php
+++ refine_block/index.php
@@ -1,11 +1,116 @@
 <!DOCTYPE html>
 <html class="no-js">
     <head>
+    	<link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="https://www.google.com/fonts#UsePlace:use/Collection:Robotohttps://www.google.com/fonts#UsePlace:use/Collection:Roboto" media="all">
+	    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" >
+	    <link rel="stylesheet/less" type="text/css" href="../bk/assets/css/quang.less" />
+	    <script   src="../assets/js/jquery-1.12.4.min.js"></script>
+	    <script src="../assets/js/less.min.js"></script>
 
     </head>
     <body>
-        <div class="html-addon-name">
+    <!-- refine_block -->
+        <div class="refine">
+        	<div class="container">
+        		<div class="row">
+        			<div class="col-lg-4">
+        				<div class="refine-colum">
+        					<ul>
+        						<li>CATEGORIES</li>
+        						<li>CLOTHING</li>
+        						<li>FOOTWEAR</li>
+            						<ul>
+            							<li>Sneakers</li>
+                                        <li>Sandals</li>   
+                                        <li>Dress</li>   
+                                        <li>Casual</li>   
+                                        <li>Boots</li> 
+            						</ul>
+        						<li>ACCESSORIES</li>
+        						<li>BRANDS</li>
+        						<li>PRICE</li>
+        						<li>COLOR</li>
+                                        <div class="select">
+                                            <div class="list-color">
+                                             <div class="color" style="background: #000"></div>
+                                             <div class="color" style="background: #F5F5DC"></div>
+                                             <div class="color" style="background: #0000FF"> <i class="fa fa-check" aria-hidden="true"></i></div>
+                                             <div class="color" style="background: #A52A2A"></div>
+                                             <div class="color" style="background: #008000"></div>
+                                             <div class="color" style="background: #808080"></div>
+                                             <div class="color" style="background: #FFA500"></div>
+                                             <div class="color" style="background: #FFC0CB"></div>
+                                             <div class="color" style="background: #800080"></div>
+                                             <div class="color" style="background: #FF0000"></div>
+                                             <div class="color" style="background: #fff"></div>
+                                             <div class="color" style="background: #FFFF00"></div>
+                                           
+                                           
+                                            </div>
+                                        </div>
+        						<li>DEALS</li>
+                                    <ul>
+                                        <li>All item on sale</li>
+                                        <li>New today</li>   
+                                        <li>New this week</li>   
+                                        <li>Free shipping</li>   
+                                        <li>Special offer</li> 
+                                        <li>Discount</li> 
+                                    </ul>
+        						<li>SIZE</li>
+                                    <div class="select" >
+                                        <div class="list-size">
+                                            <div class="size"><span>XS</span></div>
+                                            <div class="size"><span>S</span></div>
+                                            <div class="size"><span>M</span></div>
+                                            <div class="size"><span>L</span></div>
+                                            <div class="size"><span>XL</span></div>
+                                            <div class="size"><span>XXL</span></div>
+                                            <div class="size"><span>XXXL</span></div>
+                                        </div>
+                                    </div>
+        						<li>STORE</li>
+                                    <div class="brand" >
+                                    <div class="label-brand">Search Stores</div>
+                                        <div class="list-brand">
+                                            <ul>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                                <li>Brand Name</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+        						<button type="submit" form="form1" value="Submit">CLEAR FILTER</button>
+        					</ul>
+        				</div>
+        				
+        			</div>
+        			<div class="col-lg-8" style=" background: blue; height: 1000px">
+        				
+        			</div>
+        		</div>
+        		
+        	</div>
 
         </div>
+    <!-- refine_block -->
     </body>
 </html>
