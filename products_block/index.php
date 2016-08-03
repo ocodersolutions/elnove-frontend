bae918b7d298f77b39a37b7a96474bef0c5fbcf1
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-08-03 16:17:57 +0700

    success block product_item

--- products_block/index.php
+++ products_block/index.php
@@ -2,9 +2,10 @@
 <html class="no-js">
     <head>
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans" />
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 		<!-- css file for team member -->
 		<link rel="stylesheet" type="text/css" href="../assets/css/custom_product_list.less" />
+
     </head>
     <body>
         <!-- start of team member -->
@@ -12,22 +13,69 @@
 			<div class="uc_contact">
 				<img class="uc_avatar" src="product_item.jpg">
 				<div class="uc_link">
-					<a href="#" class="uc_icon" target="_blank"><img src="like_icon.jpg" alt=""></a>
-					<a href="#" class="uc_icon" target="_blank"><img src="share_icon.jpg" alt=""></a>
-					<a href="#" class="uc_icon" target="_blank"><img src="allert_icon.jpg" alt=""></a>
+					<a href="#" class="uc_icon" ><img src="like_icon.jpg" alt=""></a>
+					<a href="#" class="uc_icon" id="show" onclick="display_mode('object_share','show');"><img src="share_icon.jpg" alt=""></a>
+					<a href="#" class="uc_icon" ><img src="allert_icon.jpg" alt=""></a>
 					<a href="#" class="uc_icon buy_product"><span>BUY</span></a>
-				</div>
-				
+				</div>				
 			</div>
 			<div class="clear_float"></div>
-			<div class="share_product" style="display: none;">
-				
+			<div class="share_product" id="object_share" >
+				<i class="fa fa-times " aria-hidden="true" onclick="display_mode('object_share','hide');"></i>
+				<h2>SHARE VIA</h2>
+				<div class="share_page">
+					<a href="#">
+						<i class="fa fa-facebook fa-2x " aria-hidden="true" ></i> 
+						<span>Facebook</span>
+					</a>
+				</div>
+				<div class="clear_float"></div>
+				<div class="share_page">
+					<a href="#">
+						<i class="fa fa-pinterest-p fa-2x " aria-hidden="true"></i> 
+						<span>Pinterest</span>
+					</a>
+				</div>
+				<div class="clear_float"></div>
+				<div class="share_page">
+					<a href="#">
+						<i class="fa fa-tumblr fa-2x " aria-hidden="true"></i> 
+						<span>Twitter</span>
+					</a>
+				</div>
+				<div class="clear_float"></div>
+				<div class="share_page">
+					<a href="#">
+						<i class="fa fa-google-plus fa-2x " aria-hidden="true"></i> 
+						<span>Google +</span>
+					</a>
+				</div>
+				<div class="clear_float"></div>
+				<div class="share_page">
+					<a href="#">
+						<i class="fa fa-envelope-o fa-2x " aria-hidden="true"></i> 
+						<span>Email</span>
+					</a>
+				</div>
+					
 			</div>
+			<div class="clear_float"></div>
 			<div class="uc_info">
 				<p class="uc_job"><span>65.00 CAD </span>&nbsp;48.00 CAD</p>
 				<p>Puma Football Windbreaker</p>
 			</div>
 		</div>
 		<!-- end of team member -->
+
+		<script type="text/javascript">
+
+			function display_mode(id, mode) {
+			var e = document.getElementById(id);
+			if(mode == 'show') e.style.display = 'block';
+			else e.style.display = 'none';
+	    }
+
+		</script>
+
     </body>
 </html>
