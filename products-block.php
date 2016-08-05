4d601cbf716ab4a2410c3eca72b635d289c81ff3
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-08-05 10:20:57 +0700

    icon share of product item is complete

--- products-block.php
+++ products-block.php
@@ -17,7 +17,7 @@
             </div>
         </div>
         <div class="row ">
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
 
                 <!-- start of team member -->
                 <div class="product_item">
@@ -93,61 +93,132 @@
                 <!-- end of team member -->
 
             </div>
-            <div class="col-lg-3" id="product">
-                <div class="product">product 2</div>
+            <div class="col-lg-3" class="product">
+                <!-- start of team member -->
+                <div class="product_item">
+                    <div class="uc_contact">
+                        <img class="uc_avatar" src="assets/images/product_item.jpg">
+                        <div class="uc_link">
+                            <span class="uc_icon icon_like">
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                    </span>
+
+                            <span class="uc_icon show_item">
+                        <i class="fa fa-share-alt" aria-hidden="true"></i>
+                    </span>
+
+                            <span class="uc_icon icon_alert">
+                        <i class="fa fa-bell-o" aria-hidden="true"></i>
+                    </span>
+
+                            <span class="buy_product uc_icon">BUY</span>
+                        </div>
+                    </div>
+                    <!-- End .product_item -->
+
+                    <div class="clear_float"></div>
+
+                    <div class="share_product">
+                        <i class="fa fa-times hide_item" aria-hidden="true"></i>
+                        <h2>SHARE VIA</h2>
+                        <div class="share_page">
+                            <a href="#">
+                                <i class="fa fa-facebook fa-2x " aria-hidden="true"></i>
+                                <span>Facebook</span>
+                            </a>
+                        </div>
+                        <div class="clear_float"></div>
+                        <div class="share_page">
+                            <a href="#">
+                                <i class="fa fa-pinterest-p fa-2x " aria-hidden="true"></i>
+                                <span>Pinterest</span>
+                            </a>
+                        </div>
+                        <div class="clear_float"></div>
+                        <div class="share_page">
+                            <a href="#">
+                                <i class="fa fa-tumblr fa-2x " aria-hidden="true"></i>
+                                <span>Twitter</span>
+                            </a>
+                        </div>
+                        <div class="clear_float"></div>
+                        <div class="share_page">
+                            <a href="#">
+                                <i class="fa fa-google-plus fa-2x " aria-hidden="true"></i>
+                                <span>Google +</span>
+                            </a>
+                        </div>
+                        <div class="clear_float"></div>
+                        <div class="share_page">
+                            <a href="#">
+                                <i class="fa fa-envelope-o fa-2x " aria-hidden="true"></i>
+                                <span>Email</span>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- End .share_product -->
+
+                    <div class="clear_float"></div>
+
+                    <div class="uc_info">
+                        <p class="uc_job"><span>65.00 CAD</span>&nbsp;&nbsp;48.00 CAD</p>
+                        <p>Puma Football Windbreaker</p>
+                    </div>
+                </div>
+                <!-- end of team member -->
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 3</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 4</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 5</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 6</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 7</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 8</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 9</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 10</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 11</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 12</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 13</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 14</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 15</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 16</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 17</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 18</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 19</div>
             </div>
-            <div class="col-lg-3" id="product">
+            <div class="col-lg-3" class="product">
                 <div class="product">product 12</div>
             </div>
             <!-- <div class="row" id="load-more">
