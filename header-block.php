b645b3f4a0ee501aa19a85429fe10f5b79a22aab
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-08-05 15:36:59 +0700

    update header

--- header-block.php
+++ header-block.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; ?>
+<!-- <?php //include 'header.php'; ?> -->
 <div id="header-container" class="container-fluid hidden-xs">
     <div class="row">
         <div class="header-top-navbar col-lg-12 ">
@@ -164,4 +164,4 @@
     </div>
 </div>
 
-<?php include 'footer.php'; ?>
+<?php //include 'footer.php'; ?>
