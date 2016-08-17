7430fbe79dd24092a52d47e85ed607e917a9f666
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-08-17 10:56:17 +0700

    add class row for banner-block

--- banner-block.php
+++ banner-block.php
@@ -1,13 +1,15 @@
-<div class="col-lg-10 col-lg-offset-1 banner-content">
-    <h1>LOREM IPSUM TEXT</h1>
-    <h3>Whether for casual or business attire, get choices tailored to you.</h3>
-    <!-- <a href="#"><span class="btn btn-default">GET DRESSED</span></a> -->
-               
-    <form class="form_search">
-        <input type="search" placeholder="Search lorem ipsum dolor sit amet consectur..." name="search" class="search_input">
-        <button type="submit" class="search_submit">
-            <i class="fa fa-search" aria-hidden="true" id="search_icon"></i>
-        </button>
-    </form>
-    
+<div class="row">
+	<div class="col-lg-10 col-lg-offset-1 banner-content">
+	    <h1>LOREM IPSUM TEXT</h1>
+	    <h3>Whether for casual or business attire, get choices tailored to you.</h3>
+	    <!-- <a href="#"><span class="btn btn-default">GET DRESSED</span></a> -->
+	               
+	    <form class="form_search">
+	        <input type="search" placeholder="Search lorem ipsum dolor sit amet consectur..." name="search" class="search_input">
+	        <button type="submit" class="search_submit">
+	            <i class="fa fa-search" aria-hidden="true" id="search_icon"></i>
+	        </button>
+	    </form>
+	    
+	</div>
 </div>
